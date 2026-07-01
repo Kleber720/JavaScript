e70a2fc151bcdc23962c3da05484299ee75813a3
Kleber Amaro <kleberamarosilva@gmail.com>
2026-07-01 11:32:10 -0300

    atualização do formato do jogo 2d

---
+++ canvas/aula1/script.js
@@ -0,0 +1,83 @@
+const tela=document.querySelector("#tela");
+ 
+var context=tela.getContext("2d");
+ 
+context.fillStyle="#00f";
+ 
+//  X ; Y ; largura ; altura
+// context.fillRect(0,0,20,20);
+// context.clearRect();
+ 
+context.fillRect(0,0,20,20);
+let posicaoX=0;
+let posicaoY=0;
+
+const tamanho=40;
+
+function desenharGrid(){
+    context.strokeStyle = "#999";
+    context.font = "10px Arial";
+    context.fillStyle = "#000";
+
+    for(let y=0;y<tela.height;y+=tamanho){
+        for(let x=0;x<tela.width;x+=tamanho){
+            context.strokeRect(x,y,tamanho,tamanho);
+            context.fillText(`x:${x} y:${y}`,x,y)
+        }
+        
+    }
+    
+}
+
+function desenharPersonagem(){
+    context.clearRect(0,0,tela.width,tela.height);/* Limpando a tela*/
+    desenharGrid()
+    context.fillRect(posicaoX,posicaoY,20,20);
+    requestAnimationFrame(desenharPersonagem)
+};
+ 
+ desenharPersonagem()
+document.addEventListener("keypress",(tecla)=>{
+ 
+    //mexendo no eixo x e y do personagem para andar
+ switch(tecla.key){
+    case "d":
+        if(posicaoX+20<tela.width){
+            posicaoX+=10;
+        }
+        
+       
+        
+        break
+    case "a":
+        if(posicaoX>0){
+            posicaoX-=10;
+        }
+       
+        
+       
+        
+        break
+    case "w":
+        if(posicaoY>0){
+            posicaoY-=10;
+        }
+        
+        
+        break
+    case "s":
+        if(posicaoY+20<tela.height){
+            posicaoY+=10;
+        }
+    
+    
+    break
+    
+    default:
+        console.log("nao esta pegando")
+
+ 
+ 
+ }
+ 
+})
